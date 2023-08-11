acebec332d9fe654cf0d20df4837675a4dc33d81
Ruba <ru.sau@icloud.com>
2023-08-11 16:12:08 +0300

    Initial Commit

---
+++ learning-github/learning_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_githubApp.swift
+//  learning-github
+//
+//  Created by Ruba on 11/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct learning_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
